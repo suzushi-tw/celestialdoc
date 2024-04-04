a6d728476b4fcab046b39724a1f79db38b935ca3
suzushi-tw <ianhuang2023a@gmail.com>
2024-04-04 23:04:12 +0200

    add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,14 @@ import Header from "../components/Header";
 import Navbar from "@/components/Navbar";
 import { ClerkProvider } from '@clerk/nextjs'
 import Providers from "@/components/Providers";
-import { GeistSans } from "geist/font/sans"; 
+import { GeistSans } from "geist/font/sans";
+import Head from 'next/head';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "CelestialDOC",
   description: "open source alternative to Google drive and Docsend",
+
 };
 
 export default function RootLayout({
@@ -22,6 +24,9 @@ export default function RootLayout({
     <ClerkProvider>
       <Providers>
         <html lang="en" className={`${GeistSans.className} antialiased dark:bg-gray-950`}>
+          <Head>
+            <meta property="og:image" content={"/Demo.png"} />
+          </Head>
           <body className={inter.className} >
             <Navbar />
             {/* <Header /> */}
