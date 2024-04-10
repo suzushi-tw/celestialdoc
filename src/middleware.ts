9b2657a2a3f20196ee9d801bf93ffbdee18844fa
suzushi-tw <ianhuang2023a@gmail.com>
2024-04-10 23:45:13 +0200

    add more routes to ignored routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/',],
+  publicRoutes: ['/','/sign-in', '/sign-up'],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: ['/', '/view/(.*)'],
