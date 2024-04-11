c825ca7942758d0a6a2cc4ba6200bd590a82bb6d
suzushi-tw <ianhuang2023a@gmail.com>
2024-04-11 12:33:53 +0200

    add debug mode to midlearw

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,11 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/','/sign-in', '/sign-up'],
+  publicRoutes: ['/', '/sign-in', '/sign-up'],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: ['/', '/view/(.*)'],
+  debug: true
 });
  
 export const config = {
